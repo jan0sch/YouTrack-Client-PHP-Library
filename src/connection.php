cbc007f8d472bbbc7cdf47603946fead827af707
Jens Jahnke <jan0sch@gmx.net>
2011-03-29 16:51:48 +0200

    Added some stubs.

---
+++ src/connection.php
@@ -0,0 +1,10 @@
+<?php
+namespace YouTrack;
+/**
+ * A class for connecting to a youtrack instance.
+ *
+ * @author Jens Jahnke <jan0sch@gmx.net>
+ * Created at: 29.03.11 16:13
+ */
+class Connection {
+}
---
+++ src/youtrackclient.php
@@ -0,0 +1,135 @@
+<?php
+namespace YouTrack;
+require("connection.php");
+/**
+ * This file holds all youtrack related classes regarding data types.
+ *
+ * @author Jens Jahnke <jan0sch@gmx.net>
+ * Created at: 29.03.11 16:29
+ */
+
+/**
+ * A class extending the standard php extension.
+ */
+class YouTrackException extends Exception {
+
+}
+
+/**
+ * A class describing a youtrack object.
+ */
+class YouTrackObject {
+
+}
+
+/**
+ * A class describing a youtrack error.
+ */
+class YouTrackError {
+
+}
+
+/**
+ * A class describing a youtrack issue.
+ */
+class Issue {
+
+}
+
+/**
+ * A class describing a youtrack comment.
+ */
+class Comment {
+
+}
+
+/**
+ * A class describing a youtrack link.
+ */
+class Link {
+
+}
+
+/**
+ * A class describing a youtrack attachment.
+ */
+class Attachment {
+
+}
+
+/**
+ * A class describing a youtrack user.
+ */
+class User {
+
+}
+
+/**
+ * A class describing a youtrack group.
+ */
+class Group {
+
+}
+
+/**
+ * A class describing a youtrack role.
+ */
+class Role {
+
+}
+
+/**
+ * A class describing a youtrack project.
+ */
+class Project {
+
+}
+
+/**
+ * A class describing a youtrack subsystem.
+ */
+class Subsystem {
+
+}
+
+/**
+ * A class describing a youtrack version.
+ */
+class Version {
+
+}
+
+/**
+ * A class describing a youtrack build.
+ */
+class Build {
+
+}
+
+/**
+ * A class describing a youtrack issue link type.
+ */
+class IssueLinkType {
+
+}
+
+/**
+ * A class describing a youtrack custom field.
+ */
+class CustomField {
+
+}
+
+/**
+ * A class describing a youtrack project custom field.
+ */
+class ProjectCustomField {
+
+}
+
+/**
+ * An enum bundle.
+ */
+class EnumBundle {
+
+}
