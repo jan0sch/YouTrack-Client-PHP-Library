733d3eae8f07eb457cf971566962b47c9e28f214
Jens Jahnke <jan0sch@gmx.net>
2011-04-08 14:22:43 +0200

    Added a class for youtrack groups.

--- src/youtrackclient.php
+++ src/youtrackclient.php
@@ -222,7 +222,9 @@ class User extends YouTrackObject {
  * A class describing a youtrack group.
  */
 class Group extends YouTrackObject {
-
+  public function __construct(\SimpleXMLElement $xml = NULL, Connection $youtrack = NULL) {
+    parent::__construct($xml, $youtrack);
+  }
 }
 
 /**
