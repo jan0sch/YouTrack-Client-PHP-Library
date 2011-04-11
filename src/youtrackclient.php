8442cec84fd9afdde146a395fdd0c4761dc8e145
Jens Jahnke <jan0sch@gmx.net>
2011-04-11 14:12:11 +0200

    Added an NotImplementedException.

--- src/youtrackclient.php
+++ src/youtrackclient.php
@@ -9,6 +9,23 @@ require("connection.php");
  */
 
 /**
+ * A simple exception that should be raised if a function is not yet implemented.
+ */
+class NotImplementedException extends \Exception {
+  /**
+   * Constructor
+   *
+   * @param string $function_name The name of the function.
+   */
+  public function __construct($function_name) {
+    $code = 0;
+    $previous = NULL;
+    $message = 'This function is not yet implemented: "'. $function_name .'"!';
+    parent::__construct($message, $code, $previous);
+  }
+}
+
+/**
  * A class extending the standard php exception.
  */
 class YouTrackException extends \Exception {
