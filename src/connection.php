0236a07fcbec30bad68066f100b5cd5ca7162f33
Jens Jahnke <jan0sch@gmx.net>
2013-03-25 16:13:56 +0100

    Code Cleanup

--- src/connection.php
+++ src/connection.php
@@ -41,14 +41,14 @@ class Connection {
 
   protected function _login($login, $password) {
     curl_setopt($this->http, CURLOPT_POST, TRUE);
-    curl_setopt($this->http, CURLOPT_HTTPHEADER, array('Content-Length: 1')); //Workaround for problems when after lighthttp proxy
+    curl_setopt($this->http, CURLOPT_HTTPHEADER, array('Content-Length: 1')); // Workaround for login problems when running behind lighttpd proxy @see http://redmine.lighttpd.net/issues/1717
     curl_setopt($this->http, CURLOPT_URL, $this->base_url . '/user/login?login='. urlencode($login) .'&password='. urlencode($password));
     curl_setopt($this->http, CURLOPT_RETURNTRANSFER, TRUE);
     curl_setopt($this->http, CURLOPT_HEADER, TRUE);
     curl_setopt($this->http, CURLOPT_SSL_VERIFYPEER, $this->verify_ssl);
     curl_setopt($this->http, CURLOPT_USERAGENT, $this->user_agent);
     curl_setopt($this->http, CURLOPT_VERBOSE, $this->debug_verbose);
-    curl_setopt($this->http,CURLOPT_POSTFIELDS,"a");
+    curl_setopt($this->http,CURLOPT_POSTFIELDS, "a");
     $content = curl_exec($this->http);
     $response = curl_getinfo($this->http);
     if ((int) $response['http_code'] != 200) {
